8ec8b0de10b97d91d3f566a00f4f25e94d59fcf4
kuunamaka <mauisahashi@gmail.com>
2020-09-02 20:32:45 +0900

    some files from rumbl were created and updated

--- rumbl/lib/rumbl/accounts.ex
+++ rumbl/lib/rumbl/accounts.ex
@@ -6,14 +6,6 @@ defmodule Rumbl.Accounts do
   alias Rumbl.Repo
   alias Rumbl.Accounts.User
 
-  #def list_users do
-  #  [
-  #    %User{id: "1", name: "José", username: "josevalim"},
-  #    %User{id: "2", name: "Bruce", username: "redrapids"},
-  #    %User{id: "3", name: "Chris", username: "chrismccord"}    
-  #  ]
-  #end
-
   def get_user(id) do
     Repo.get(User, id)
   end
@@ -41,5 +33,31 @@ defmodule Rumbl.Accounts do
     %User{} # start with an empty user
     |> User.changeset(attrs) # applies a changeset
     |> Repo.insert() # insert it to the Repo
+  end
+
+  # accounts.change1.ex
+  def change_registration(%User{} = user, params) do
+    User.registration_changeset(user, params)
+  end
+
+  def register_user(attrs \\ %{}) do
+    %User{}
+    |> User.registration_changeset(attrs)
+    |> Repo.insert()
+  end
+
+  # accounts.change2.ex
+  def authenticate_by_username_and_pass(username, given_pass) do
+    user = get_user_by(username: username)
+
+    cond do
+      user && Pbkdf2.verify_pass(given_pass, user.password_hash) ->
+        {:ok, user}
+      user ->
+        {:error, :unauthorized}
+      true ->
+        Pbkdf2.no_user_verify()
+        {:error, :not_found}
+    end
   end
 end
--- rumbl/lib/rumbl/accounts/user.ex
+++ rumbl/lib/rumbl/accounts/user.ex
@@ -2,9 +2,12 @@ defmodule Rumbl.Accounts.User do
   use Ecto.Schema
   import Ecto.Changeset
 
+  # user.change1.ex
   schema "users" do
     field :name, :string
     field :username, :string
+    field :password, :string, virtual: true
+    field :password_hash, :string
 
     timestamps()
   end
@@ -15,5 +18,23 @@ defmodule Rumbl.Accounts.User do
     |> cast(attrs, [:name, :username])
     |> validate_required([:name, :username])
     |> validate_length(:username, min: 1, max: 20)
+  end
+
+  # user.change1.ex
+  def registration_changeset(user, params) do
+    user
+    |> changeset(params)
+    |> cast(params, [:password], [])
+    |> validate_length(:password, min: 6, max: 100)
+    |> put_pass_hash()
+  end
+
+  def put_pass_hash(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: pass}} ->
+        put_change(changeset, :password_hash, Pbkdf2.hash_pwd_salt(pass))
+      _ ->
+        changeset
+    end
   end
 end
